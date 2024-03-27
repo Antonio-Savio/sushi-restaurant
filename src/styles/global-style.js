336fc5532fee159b10f622621b60f0e677cb36db
Antonio-Savio <antonio.savio28@gmail.com>
2024-03-27 00:29:52 -0300

    add button global styling - refactor

--- src/styles/global-style.js
+++ src/styles/global-style.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   :root {
@@ -22,4 +22,21 @@ export const GlobalStyle = createGlobalStyle`
     text-decoration: none;
     color: inherit;
   }
+`
+
+export const buttonStyle = css`
+    background-color: var(--main-color);
+    text-align: center;
+    border-radius: 8px;
+    font-weight: 500;
+    font-size: inherit;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    cursor: pointer;
+    transition: var(--time);
+
+    &:hover {
+        color: #fff;
+        filter: brightness(110%);
+    }
 `
